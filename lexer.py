1b04ec686c03dee6ed0fc17aad43ea145a465f53
qinggang <qinggang22@gmail.com>
2020-07-09 15:10:05 +0800

    更新词法分析器

--- lexer.py
+++ lexer.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 class TokenKind(Enum):
     EOF = 0            # end-of-file
@@ -65,54 +66,6 @@ class TokenKind(Enum):
     OP_BNOT = OP_WAVE
     OP_BXOR = OP_WAVE
 
-keywords_tokens = {
-"and":      TokenKind.OP_AND,
-"break":    TokenKind.KW_BREAK,
-"do":       TokenKind.KW_DO,
-"else":     TokenKind.KW_ELSE,
-"elseif":   TokenKind.KW_ELSEIF,
-"end":      TokenKind.KW_END,
-"false":    TokenKind.KW_FALSE,
-"for":      TokenKind.KW_FOR,
-"function": TokenKind.KW_FUNCTION,
-"goto":     TokenKind.KW_GOTO,
-"if":       TokenKind.KW_IF,
-"in":       TokenKind.KW_IN,
-"local":    TokenKind.KW_LOCAL,
-"nil":      TokenKind.KW_NIL,
-"not":      TokenKind.OP_NOT,
-"or":       TokenKind.OP_OR,
-"repeat":   TokenKind.KW_REPEAT,
-"return":   TokenKind.KW_RETURN,
-"then":     TokenKind.KW_THEN,
-"true":     TokenKind.KW_TRUE,
-"until":    TokenKind.KW_UNTIL,
-"while":    TokenKind.KW_WHILE
-}
-
-single_symbol_tokens = {
-';':        TokenKind.SEP_SEMI,
-',':        TokenKind.SEP_COMMA,
-'(':        TokenKind.SEP_LPAREN,
-')':        TokenKind.SEP_RPAREN,
-']':        TokenKind.SEP_RBRACK,
-'{':        TokenKind.SEP_LCURLY,
-'}':        TokenKind.SEP_RCURLY,
-'+':        TokenKind.OP_ADD,
-'-':        TokenKind.OP_MINUS,
-'*':        TokenKind.OP_MUL,
-'^':        TokenKind.OP_POW,
-'%':        TokenKind.OP_MOD,
-'&':        TokenKind.OP_BAND,
-'|':        TokenKind.OP_BOR,
-'#':        TokenKind.OP_LEN,
-}
-
-special_symbols = {
-    'new_line': ['\r', '\n'],
-    'white_space': ['\t', '\n', '\v', '\f', '\r', ' ']
-}
-
 class Token:
     def __init__(self, kind, line, data):
         self.kind = kind
@@ -122,8 +75,59 @@ class Token:
     def __str__(self):
         return "{kind:'%s', line:'%d', data:'%s'}" % (self.kind.name, self.line, self.data)
 
-
 class Lexer:
+    re_Identifier = re.compile(r'[_A-Za-z][_A-Za-z0-9]*')
+    re_number = re.compile(r"0[xX][0-9a-fA-F]*(\.[0-9a-fA-F]*)?([pP][+\-]?[0-9]+)?|"
+                           r"[0-9]*(\.[0-9]*)?([eE][+\-]?[0-9]+)?")
+
+    keywords_tokens = {
+        "and":      TokenKind.OP_AND,
+        "break":    TokenKind.KW_BREAK,
+        "do":       TokenKind.KW_DO,
+        "else":     TokenKind.KW_ELSE,
+        "elseif":   TokenKind.KW_ELSEIF,
+        "end":      TokenKind.KW_END,
+        "false":    TokenKind.KW_FALSE,
+        "for":      TokenKind.KW_FOR,
+        "function": TokenKind.KW_FUNCTION,
+        "goto":     TokenKind.KW_GOTO,
+        "if":       TokenKind.KW_IF,
+        "in":       TokenKind.KW_IN,
+        "local":    TokenKind.KW_LOCAL,
+        "nil":      TokenKind.KW_NIL,
+        "not":      TokenKind.OP_NOT,
+        "or":       TokenKind.OP_OR,
+        "repeat":   TokenKind.KW_REPEAT,
+        "return":   TokenKind.KW_RETURN,
+        "then":     TokenKind.KW_THEN,
+        "true":     TokenKind.KW_TRUE,
+        "until":    TokenKind.KW_UNTIL,
+        "while":    TokenKind.KW_WHILE
+    }
+
+    single_symbol_tokens = {
+        ';':        TokenKind.SEP_SEMI,
+        ',':        TokenKind.SEP_COMMA,
+        '(':        TokenKind.SEP_LPAREN,
+        ')':        TokenKind.SEP_RPAREN,
+        ']':        TokenKind.SEP_RBRACK,
+        '{':        TokenKind.SEP_LCURLY,
+        '}':        TokenKind.SEP_RCURLY,
+        '+':        TokenKind.OP_ADD,
+        '-':        TokenKind.OP_MINUS,
+        '*':        TokenKind.OP_MUL,
+        '^':        TokenKind.OP_POW,
+        '%':        TokenKind.OP_MOD,
+        '&':        TokenKind.OP_BAND,
+        '|':        TokenKind.OP_BOR,
+        '#':        TokenKind.OP_LEN,
+    }
+
+    special_symbols = {
+        'new_line': ['\r', '\n'],
+        'white_space': ['\t', '\n', '\v', '\f', '\r', ' ']
+    }
+
     def __init__(self, chunk, file_name):
         self.chunk = chunk
         self.file_name = file_name
@@ -152,9 +156,9 @@ class Lexer:
         if self.cur_pos >= len(self.chunk):
             return Token(TokenKind.EOF, self.cur_line, None)
         c = self.chunk[self.cur_pos]
-        if c in single_symbol_tokens:
+        if c in self.single_symbol_tokens:
             self.move_point(1)
-            return Token(single_symbol_tokens[c], self.cur_line, c)
+            return Token(self.single_symbol_tokens[c], self.cur_line, c)
         elif c == ':':
             if self.is_start_with('::'):
                 self.move_point(2)
@@ -229,10 +233,10 @@ class Lexer:
             elif self.is_start_with('\r\n') or self.is_start_with('\n\r'):
                 self.move_point(2)
                 self.cur_line = self.cur_line + 1
-            elif self.chunk[self.cur_pos] in special_symbols['new_line']:
+            elif self.chunk[self.cur_pos] in self.special_symbols['new_line']:
                 self.move_point(1)
                 self.cur_line = self.cur_line + 1
-            elif self.chunk[self.cur_pos] in special_symbols['white_space']:
+            elif self.chunk[self.cur_pos] in self.special_symbols['white_space']:
                 self.move_point(1)
             else:
                 break
@@ -244,8 +248,22 @@ class Lexer:
         return self.chunk.startswith(prefix, self.cur_pos)
     
     def scan_number(self):
-        return Token(TokenKind.NUMBER, self.cur_line, 12)
+        token = self.re_match(self.re_number)
+        self.move_point(len(token))
+        return Token(TokenKind.NUMBER, self.cur_line, token)
     
+    def re_match(self, pattern):
+        m = pattern.match(self.chunk, self.cur_pos)
+        if m:
+            token = m.group()
+            return token
+        raise Exception('unreachable')
+
+    def scan_identifier(self):
+        token = self.re_match(self.re_Identifier)
+        self.move_point(len(token))
+        return Token(TokenKind.IDENTIFIER, self.cur_line, token)
+
     def scan_long_string(self):
         return Token(TokenKind.STRING, self.cur_line, "")
 
@@ -262,127 +280,3 @@ class Lexer:
         if base==16:
             return (num_str>='0' and num_str<='9') or (num_str>='a' and num_str<='f')
         return False
-
-#    def get_token(self):
-#        try:
-#            while True:
-#                if self.cur_char in [' ', '\t', '\n']:
-#                    if self.cur_char == '\n':
-#                        self.cur_line = self.cur_line + 1
-#                    self.get_char()
-#                    continue
-#                break
-#            # 标识符
-#            if self.cur_char == '_' or self.cur_char.isalpha():
-#                cur_word = self.cur_char
-#                while True:
-#                    prefetch = self.prefech_char()
-#                    if prefetch == '_' or prefetch.isalpha() or prefetch.isdigit():
-#                        self.get_char()
-#                        cur_word = cur_word+self.cur_char
-#                        continue
-#                    break
-#                if cur_word in self.reserve_words:
-#                    return self.reserve_words[cur_word]
-#                return Token(Tag.ID, cur_word)
-#            # 操作符
-#            elif self.cur_char in self.symbols_all_begins:
-#                cur_word = self.cur_char
-#                if cur_word == '-' and self.prefech_char()=='-':
-#                    while True:
-#                        self.get_char()
-#                        if self.cur_char=='\n':
-#                            self.cur_line  = self.cur_line+1
-#                            break
-#                    return self.get_token()
-#                if cur_word+self.prefech_char() in self.symbols_2:
-#                    self.get_char()
-#                    cur_word_2 = cur_word+self.cur_char
-#                    if cur_word_2+self.prefech_char() in self.symbols_3:
-#                        self.get_char()
-#                        return Token(Tag.SYMBOL, cur_word_2+self.cur_char)
-#                    return Token(Tag.SYMBOL, cur_word_2)
-#                if cur_word+self.prefech_char(1, 2) in self.symbols_3:
-#                    self.get_char(2)
-#                    return Token(Tag.SYMBOL, cur_word+self.cur_char)
-#                return Token(Tag.SYMBOL, cur_word)
-#            # 字符串
-#            elif self.cur_char in ["'", '"']:
-#                curr_str = ""
-#                end_str = self.cur_char
-#                while True:
-#                    self.get_char()
-#                    if self.cur_char == end_str:
-#                        return Token(Tag.STRING, curr_str)
-#                    else:
-#                        curr_str = curr_str+self.cur_char
-#            # 数字
-#            elif self.isdigit(self.cur_char, 10):
-#                num_str = self.cur_char
-#                base = 10
-#                tag = Tag.INTEGER
-#                num_val = int(self.cur_char, base)
-#                while True:
-#                    prefetch = self.prefech_char().lower()
-#                    if self.isdigit(prefetch, base):
-#                        self.get_char()
-#                        num_str = num_str+self.cur_char
-#                        num_val = num_val*base+int(self.cur_char, base)
-#                        continue
-#                    if num_str=='0' and prefetch.lower() in ['x', 'b', 'o']:
-#                        if prefetch == 'x':
-#                            base = 16
-#                        if prefetch == 'b':
-#                            base = 2
-#                        if prefetch == 'o':
-#                            base = 8
-#                        self.get_char()
-#                        num_str = num_str+self.cur_char
-#                        continue
-#                    break
-#                if (base==16 or base==10) and self.prefech_char()=='.':
-#                    tag = Tag.FLOAT
-#                    self.get_char()
-#                    num_str = num_str+self.cur_char
-#                    d = base
-#                    while True:
-#                        if self.isdigit(self.prefech_char(), base):
-#                            self.get_char()
-#                            num_val = num_val+int(self.cur_char, base)/d
-#                            d = d*base
-#                            num_str = num_str+self.cur_char
-#                            continue
-#                        break
-#                if base==10 and self.prefech_char().lower()=='e':
-#                    tag = Tag.FLOAT
-#                    self.get_char()
-#                    num_str = num_str+self.cur_char
-#                    d = base
-#                    if self.prefech_char()=='-':
-#                        d = 1/base
-#                        self.get_char()
-#                        num_str = num_str+self.cur_char
-#                    if self.prefech_char()=='+':
-#                        self.get_char()
-#                    e = 0 
-#                    while True:
-#                        if self.isdigit(self.prefech_char(), base):
-#                            self.get_char()
-#                            e = e*base+int(self.cur_char)
-#                            num_str = num_str+self.cur_char
-#                            continue
-#                        break
-#                    num_val = num_val*d**e
-#                return Token(tag, num_val)
-#            elif self.cur_char == '':
-#                return None
-#            else:
-#                raise Exception("unknown char: %s" % self.cur_char)
-#        except Exception as err:
-#            print("get_token except: "+str(err))
-#            return None
-#        finally:
-#            self.cur_char = ' '
-#
-#def get_lexer(src_code):
-#    return TokenIter(src_code)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lexer
 
 src_code = """
+3   345   0xff   0xBEBADA 3.0     3.1416     314.16e-2     0.31416E1     34e1
 ; . .  ... |
+print(...)
 <<
 >>
 """
